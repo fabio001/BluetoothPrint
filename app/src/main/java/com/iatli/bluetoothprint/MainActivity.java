f52a153255f8d34a5753d61d6f7e5ec7d4762d0f
ibrahim <ibrahim6551@hotmail.com>
2021-06-19 00:23:43 +0300

    Webview functionality improved. Printing button is removed. Directly understands from URL.

--- app/src/main/java/com/iatli/bluetoothprint/MainActivity.java
+++ app/src/main/java/com/iatli/bluetoothprint/MainActivity.java
@@ -9,22 +9,23 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
-import com.dantsu.printerthermal_escpos_bluetooth.Printer;
-import com.dantsu.printerthermal_escpos_bluetooth.bluetooth.BluetoothPrinterSocketConnection;
-import com.dantsu.printerthermal_escpos_bluetooth.bluetooth.BluetoothPrinters;
-import com.dantsu.printerthermal_escpos_bluetooth.textparser.PrinterTextParserImg;;
-import android.util.DisplayMetrics;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG ="RAFY";
+    private static final String TAG ="YENORSAN";
     private static final String BASE_URL = "http://rafetdurgut.com/Yenorsan/";
     public static final int PERMISSION_BLUETOOTH = 1551;
+    private static final String HIDDEN_URL_NAME = "print";
+
+    private BluetoothPrinter bluetoothPrinter=null;
 
     private WebView webView=null;
 
@@ -32,10 +33,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webview);
 
+        webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient(){
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(url.contains(HIDDEN_URL_NAME)){
+                    if(bluetoothPrinter == null){
+                        bluetoothPrinter = new BluetoothPrinter();
+                    }
+                    //get the formatted text from URL
+                    Toast.makeText(MainActivity.this, getString(R.string.printing), Toast.LENGTH_SHORT).show();
+                    printUrl(url);
+                    return true;
+                }
                 view.loadUrl(url);
                 return true;
             }
@@ -43,105 +53,62 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "App is loaded");
         webView.loadUrl(BASE_URL);
+
+
     }
 
-    public void printIt() {
-
+    //public void yazdir(View view) {
+    private void printUrl(String urlToPrint){
+        Log.d(TAG, "Printing url "+ urlToPrint);
+        //request bluetooth permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, "Bluetooth permission is sent");
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH}, MainActivity.PERMISSION_BLUETOOTH);
         } else {
-            Log.d(TAG, "Bluetooth permission is OK. Printing");
-            BluetoothPrinterSocketConnection bsocket =  BluetoothPrinters.selectFirstPairedBluetoothPrinter();
-            if(bsocket == null) {
-                Toast.makeText(this, "Bluetooth socketi boş!", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            Printer printer = new Printer(bsocket, 203, 48f, 32);
-            if(printer == null){
-                Toast.makeText(this, "Printer objesi oluşturulamıyor!", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            printer.printFormattedText(
-                            //"[C]<img>" + PrinterTextParserImg.bitmapToHexadecimalString(printer, this.getApplicationContext().getResources().getDrawableForDensity(R.drawable.ic_baseline_import_export_24, DisplayMetrics.DENSITY_MEDIUM)) + "</img>\n" +
-                                    "[L]\n" +
-                                    "[C]<u><font size='big'>RAFET RAPORLAMA</font></u>\n" +
-                                    "[L]\n" +
-                                    "[C]================================\n" +
-                                    "[L]\n" +
-                                    "[L]<b>RAPOR MALZEME 1</b>[R]123\n" +
-                                    "[L]  + Kalan : 222\n" +
-                                    "[L]\n" +
-                                    "[L]<b>Rapor Malzeme 2</b>[R]55\n" +
-                                    "[L]  + Kalan : 10\n" +
-                                    "[L]\n" +
-                                    "[C]--------------------------------\n" +
-                                    "[R]TOPLAM:[R]178\n" +
-                                    "[R]Kalan:[R]232\n" +
-                                    "[L]\n" +
-                                    "[C]================================\n" +
-                                    "[L]\n" +
-                                    "[L]<font size='tall'>Geliştirici :</font>\n" +
-                                    "[L]Rafet DURGUT\n" +
-                                    "[L]www.rafy.com\n" +
-                                    "[L]Tel : +90555555555\n"
-                    );
+            Log.d(TAG, "Bluetooth is OK. Now downloading text from given URL");
 
-            Log.d(TAG, "printing function done");
+            Ion.with(this)
+                    .load(urlToPrint)
+                    .asJsonObject()
+                    .setCallback(new FutureCallback<JsonObject>() {
+                @Override
+                public void onCompleted(Exception e, JsonObject result) {
+                   if(result != null){
+                       boolean isSuccess = result.get("success").getAsBoolean();
+                       if(isSuccess) {
+                           String formattedText = result.get("print").getAsString();
 
+                           if(bluetoothPrinter == null){
+                               bluetoothPrinter = new BluetoothPrinter();
+                           }
+
+                           int numDevices = bluetoothPrinter.getBluetoothPrinterCount();
+                           if(numDevices<=0){
+                               Toast.makeText(MainActivity.this, getString(R.string.bluetoothpair), Toast.LENGTH_SHORT).show();
+                               gotoBluetoothmenu();
+                               return;
+                           }
+                           Log.d(TAG, "Printing the report...");
+                           bluetoothPrinter.printOnDevice(formattedText);
+                           Toast.makeText(MainActivity.this,getString(R.string.printed), Toast.LENGTH_SHORT).show();
+
+
+
+                       }
+                       else{
+                           String errorMessage = result.get("error").getAsString();
+                           Toast.makeText(MainActivity.this, errorMessage,Toast.LENGTH_SHORT).show();
+                       }
+                   }
+                   else{
+                       Log.d(TAG, "result null geliyor.");
+                       Toast.makeText(MainActivity.this, getString(R.string.warnuser), Toast.LENGTH_LONG).show();
+
+                   }
+                }
+            });
 
         }
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        Log.d(TAG, "Bluetooth permission retrieved");
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            switch (requestCode) {
-                case MainActivity.PERMISSION_BLUETOOTH:
-                    Toast.makeText(this,"Bluetooth izni verildi", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Bluetooth permission granted");
-                    break;
-            }
-        }
-        else if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED){
-            switch (requestCode) {
-                case MainActivity.PERMISSION_BLUETOOTH:
-                    Toast.makeText(this,"Bluetooth yazici kullanmak için izin vermelisiniz!", Toast.LENGTH_LONG).show();
-                    Log.d(TAG, "Bluetooth permission not granted");
-                    break;
-            }
-        }
-    }
-
-    public void yazdir(View view) {
-        Log.d(TAG, "Print clicked");
-        if(getBluetoothPrinterCount()>0)
-            printIt();
-        else {
-            Toast.makeText(this, "İlk önce bir bluetooth yazici eşleştirin.", Toast.LENGTH_SHORT).show();
-            gotoBluetoothmenu();
-        }
-    }
-
-    public void kontrolet(View view) {
-        BluetoothPrinters b = new BluetoothPrinters();
-        BluetoothPrinterSocketConnection array[] = b.getList();
-        if(array==null ){
-            Toast.makeText(this, "Bluetooth yazici bulunamadi." , Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Toast.makeText(this, "Bluetooth Yazıcı Sayısı: "+ array.length , Toast.LENGTH_SHORT).show();
-    }
-
-    public int getBluetoothPrinterCount(){
-        BluetoothPrinters b = new BluetoothPrinters();
-        BluetoothPrinterSocketConnection array[] = b.getList();
-        if(array==null )
-            return 0;
-        return array.length;
     }
 
     public void gotoBluetoothmenu(){
@@ -155,5 +122,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        Log.d(TAG, "Bluetooth permission retrieved");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                case MainActivity.PERMISSION_BLUETOOTH:
+                    Toast.makeText(this,getString(R.string.bluetoothpermgrant), Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Bluetooth permission granted");
+                    break;
+            }
+        }
+        else if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED){
+            switch (requestCode) {
+                case MainActivity.PERMISSION_BLUETOOTH:
+                    Toast.makeText(this, getString(R.string.bluetoothpermdenied), Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Bluetooth permission not granted");
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
 
 }
